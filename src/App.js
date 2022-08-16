62eb9f26850d4f46487f03c7207b1d021d186fbf
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-08-16 13:31:31 -0400

    Github and Portfolio Links Added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ function App() {
   return (
     <div className="App">
       <div className="title">English Dictionary</div>
+      <p className="footer">
+        This is an open-source{" "}
+        <a
+          href="https://github.com/courtneyyatteau/dictionary-react-app"
+          target="_blank"
+          rel="noreferrer"
+          className="App-link"
+        >
+          Github project
+        </a>{" "}
+        created by{" "}
+        <a
+          href="https://courtney-yatteau-portfolio.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+          className="App-link"
+        >
+          Courtney Yatteau
+        </a>
+        .
+      </p>
     </div>
   );
 }
